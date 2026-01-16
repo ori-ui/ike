e61ba5b03385e664980153d9fbd73b8b08683ef2
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2026-01-16 17:06:59 +0100

    add: on_key and on_pointer_button

--- ike-ori/src/views/window.rs
+++ ike-ori/src/views/window.rs
@@ -1,4 +1,7 @@
-use ike_core::{AnyWidgetId, Builder, Color, KeyEvent, PointerEvent, Size, WindowId, WindowSizing};
+use ike_core::{
+    AnyWidgetId, Builder, Color, Key, KeyEvent, KeyPressEvent, Modifiers, PointerButton,
+    PointerButtonEvent, PointerEvent, Size, WindowId, WindowSizing,
+};
 use ori::{Action, Event, NoElement, Provider, Proxied, Proxy, View, ViewMarker};
 
 use crate::Palette;
@@ -7,10 +10,13 @@ pub fn window<V, T>(contents: V) -> Window<V, T> {
     Window::new(contents)
 }
 
-#[allow(
-    clippy::type_complexity,
-    reason = "it's more clear to have the long types"
-)]
+type KeyFilter = Box<dyn FnMut(&KeyEvent) -> bool>;
+type PointerFilter = Box<dyn FnMut(&PointerEvent) -> bool>;
+type OnKeyEvent<T> = Box<dyn FnMut(&mut T, &KeyEvent) -> Action>;
+type OnPointerEvent<T> = Box<dyn FnMut(&mut T, &PointerEvent) -> Action>;
+type OnKey<T> = Box<dyn FnMut(&mut T, &KeyPressEvent) -> Action>;
+type OnPointer<T> = Box<dyn FnMut(&mut T, &PointerButtonEvent) -> Action>;
+
 pub struct Window<V, T> {
     contents:       V,
     title:          String,
@@ -18,10 +24,12 @@ pub struct Window<V, T> {
     visible:        bool,
     decorated:      bool,
     color:          Option<Color>,
-    key_filter:     Option<Box<dyn FnMut(&KeyEvent) -> bool>>,
-    pointer_filter: Option<Box<dyn FnMut(&PointerEvent) -> bool>>,
-    on_key:         Option<Box<dyn FnMut(&mut T, &KeyEvent) -> Action>>,
-    on_pointer:     Option<Box<dyn FnMut(&mut T, &PointerEvent) -> Action>>,
+    key_filter:     Option<KeyFilter>,
+    pointer_filter: Option<PointerFilter>,
+    on_key:         Option<OnKeyEvent<T>>,
+    on_pointer:     Option<OnPointerEvent<T>>,
+    on_keys:        Vec<(Key, Modifiers, OnKey<T>)>,
+    on_pointers:    Vec<(PointerButton, OnPointer<T>)>,
 }
 
 impl<V, T> Window<V, T> {
@@ -41,6 +49,8 @@ impl<V, T> Window<V, T> {
             pointer_filter: None,
             on_key: None,
             on_pointer: None,
+            on_keys: Vec::new(),
+            on_pointers: Vec::new(),
         }
     }
 
@@ -84,7 +94,51 @@ impl<V, T> Window<V, T> {
         self
     }
 
+    /// Register an [`Action`] callback for when a `key` is pressed with `modifiers` held.
+    ///
+    /// This is useful for registering keyboard shortcuts. Multiple keys can be set for one window.
     pub fn on_key<A>(
+        mut self,
+        key: impl Into<Key>,
+        modifiers: Modifiers,
+        mut on_key: impl FnMut(&mut T, &KeyPressEvent) -> A + 'static,
+    ) -> Self
+    where
+        A: Into<Action>,
+    {
+        self.on_keys.push((
+            key.into(),
+            modifiers,
+            Box::new(move |data, event| on_key(data, event).into()),
+        ));
+        self
+    }
+
+    /// Register an [`Action`] callback for when a pointer `button` is pressed.
+    ///
+    /// This is useful for registering shortcuts, like pressing [`PointerButton::Backward`] or
+    /// [`PointerButton::Forward`]. Multiple pointer buttons can be set for one window.
+    pub fn on_pointer_button<A>(
+        mut self,
+        button: PointerButton,
+        mut on_button: impl FnMut(&mut T, &PointerButtonEvent) -> A + 'static,
+    ) -> Self
+    where
+        A: Into<Action>,
+    {
+        self.on_pointers.push((
+            button,
+            Box::new(move |data, event| on_button(data, event).into()),
+        ));
+        self
+    }
+
+    /// Regster an [`Action`] callback for when a [`KeyEvent`] is emitted.
+    ///
+    /// `filter` allows for choosing which events to handle, this matters for platforms like
+    /// `android` where the operating system needs to know whether a key press like _audio down_ or
+    /// _back_ is handled by the application.
+    pub fn on_key_event<A>(
         mut self,
         filter: impl FnMut(&KeyEvent) -> bool + 'static,
         mut on_key: impl FnMut(&mut T, &KeyEvent) -> A + 'static,
@@ -99,7 +153,8 @@ impl<V, T> Window<V, T> {
         self
     }
 
-    pub fn on_pointer<A>(
+    /// Regster an [`Action`] callback for when a [`PointerEvent`] is emitted.
+    pub fn on_pointer_event<A>(
         mut self,
         filter: impl FnMut(&PointerEvent) -> bool + 'static,
         mut on_pointer: impl FnMut(&mut T, &PointerEvent) -> A + 'static,
@@ -112,6 +167,64 @@ impl<V, T> Window<V, T> {
             on_pointer(data, event).into()
         }));
         self
+    }
+
+    fn register_on_key(&mut self, cx: &mut (impl Builder + Proxied), id: WindowId) {
+        let proxy = cx.proxy();
+        if let Some(window) = cx.world_mut().get_window_mut(id) {
+            let mut filter = self.key_filter.take();
+            let keys: Vec<_> = self
+                .on_keys
+                .iter()
+                .map(|(key, mods, _)| (key.clone(), *mods))
+                .collect();
+
+            window.set_on_key(Box::new(move |event| {
+                let is_key = match event {
+                    KeyEvent::Down(press) => keys.iter().any(|(key, mods)| {
+                        let is_key = press.key == *key;
+                        let is_mods = press.modifiers == *mods;
+
+                        is_key && is_mods
+                    }),
+                    _ => false,
+                };
+
+                if is_key || filter.as_mut().is_some_and(|filter| filter(event)) {
+                    proxy.event(Event::new(
+                        WindowEvent::Key(id, event.clone()),
+                        None,
+                    ));
+
+                    true
+                } else {
+                    false
+                }
+            }));
+        }
+
+        let proxy = cx.proxy();
+        if let Some(window) = cx.world_mut().get_window_mut(id) {
+            let buttons: Vec<_> = self.on_pointers.iter().map(|(btn, _)| *btn).collect();
+            let mut filter = self.pointer_filter.take();
+
+            window.set_on_pointer(Box::new(move |event| {
+                let is_button = match event {
+                    PointerEvent::Down(press) => buttons.contains(&press.button),
+                    _ => false,
+                };
+
+                if is_button || filter.as_mut().is_some_and(|filter| filter(event)) {
+                    proxy.event(Event::new(
+                        WindowEvent::Pointer(id, event.clone()),
+                        None,
+                    ));
+                    true
+                } else {
+                    false
+                }
+            }));
+        }
     }
 }
 
@@ -135,54 +248,15 @@ where
         let palette = cx.get_or_default::<Palette>();
         let id = cx.world_mut().create_window(contents.upcast());
 
+        let color = self.color.unwrap_or(palette.background);
+
         cx.set_window_title(id, self.title.clone());
         cx.set_window_sizing(id, self.sizing);
         cx.set_window_visible(id, self.visible);
         cx.set_window_decorated(id, self.decorated);
-        cx.set_window_color(
-            id,
-            self.color.unwrap_or(palette.background),
-        );
+        cx.set_window_color(id, color);
 
-        let proxy = cx.proxy();
-
-        if let Some(window) = cx.world_mut().get_window_mut(id) {
-            let proxy = proxy.cloned();
-            let mut filter = self.key_filter.take();
-
-            window.set_on_key(Box::new(move |event| {
-                if let Some(ref mut filter) = filter
-                    && filter(event)
-                {
-                    proxy.event(Event::new(
-                        WindowEvent::Key(id, event.clone()),
-                        None,
-                    ));
-                    true
-                } else {
-                    false
-                }
-            }));
-        }
-
-        if let Some(window) = cx.world_mut().get_window_mut(id) {
-            let mut filter = self.pointer_filter.take();
-
-            window.set_on_pointer(Box::new(move |event| {
-                if let Some(ref mut filter) = filter
-                    && filter(event)
-                {
-                    proxy.event(Event::new(
-                        WindowEvent::Pointer(id, event.clone()),
-                        None,
-                    ));
-                    true
-                } else {
-                    false
-                }
-            }));
-        }
-
+        self.register_on_key(cx, id);
         (NoElement, (id, contents, state))
     }
 
@@ -201,6 +275,8 @@ where
             data,
             &mut old.contents,
         );
+
+        self.register_on_key(cx, *id);
 
         let palette = cx.get_or_default::<Palette>();
 
@@ -228,10 +304,8 @@ where
         }
 
         if self.color != old.color {
-            cx.set_window_color(
-                *id,
-                self.color.unwrap_or(palette.background),
-            );
+            let color = self.color.unwrap_or(palette.background);
+            cx.set_window_color(*id, color);
         }
     }
 
@@ -255,6 +329,15 @@ where
     ) -> Action {
         match event.get() {
             Some(WindowEvent::Key(target, event)) if target == id => {
+                for (key, mods, on_key) in &mut self.on_keys {
+                    if let KeyEvent::Down(event) = event
+                        && event.key == *key
+                        && event.modifiers == *mods
+                    {
+                        return on_key(data, event);
+                    }
+                }
+
                 return if let Some(ref mut on_key) = self.on_key {
                     on_key(data, event)
                 } else {
@@ -263,6 +346,14 @@ where
             }
 
             Some(WindowEvent::Pointer(target, event)) if target == id => {
+                for (button, on_button) in &mut self.on_pointers {
+                    if let PointerEvent::Down(event) = event
+                        && event.button == *button
+                    {
+                        return on_button(data, event);
+                    }
+                }
+
                 return if let Some(ref mut on_pointer) = self.on_pointer {
                     on_pointer(data, event)
                 } else {
